55fdca06e4b31dfbc0e7862289f2113e68b3a46d
CeShine Lee <shuanck@gmail.com>
2019-06-08 18:26:27 +0800

    Update

--- config.fish
+++ config.fish
@@ -1,4 +1,9 @@
 set -x PATH $PATH /home/ceshine/miniconda3/bin
 
 source /home/ceshine/miniconda3/etc/fish/conf.d/conda.fish
-set -gx CONDA_LEFT_PROMPT 1
+# set -gx CONDA_LEFT_PROMPT 1
+
+set -g theme_date_format "+%Y-%m-%d %H:%M"
+set -g theme_color_scheme solarized-dark
+
+set -g NVM_DIR "/mnt/SSD_Data/nvm"
